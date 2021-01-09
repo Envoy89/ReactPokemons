c721179ca0a85e83f3bec357f185d45c0689b364
Minyazev Ruzil <ruzil33@gmail.com>
2021-01-09 21:42:56 +0300

    Add eslint, prettier & husky

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import cn from 'classnames';
+
+import s from './App.module.scss';
+
+const App = () => {
+  <div className={cn(s.header, 'color')}>This is App component!!!!</div>;
+};
+
+export default App;
