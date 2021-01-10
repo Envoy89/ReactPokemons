d12086402adf6686397330930fe0f7e87cc6d94a
Minyazev Ruzil <ruzil33@gmail.com>
2021-01-10 17:31:14 +0300

    add style and structure for Header

---
+++ client/src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../components/Header';
+
+import s from './Home.module.scss';
+
+const HomePage = () => (
+  <div className={s.root}>
+    <Header />
+    Home!!!
+  </div>
+);
+
+export default HomePage;
