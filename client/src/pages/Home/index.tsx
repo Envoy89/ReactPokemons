fd9ae3626e192b163500153ca5e6bde2e12aaaee
Minyazev Ruzil <ruzil33@gmail.com>
2021-01-11 22:44:52 +0300

    first try home page styling

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import Button from '../../components/Button';
+import Footer from '../../components/Footer';
 import Header from '../../components/Header';
+import Layout from '../../components/Layout';
+import Parallax from '../../components/Parallax';
 
 import s from './Home.module.scss';
 
 const HomePage = () => (
   <div className={s.root}>
     <Header />
-    Home!!!
+    <Layout className={s.layout}>
+      <div className={s.layoutText}>
+        <div className={s.textTitle}>
+          <b>Find</b> all your favorite <b>Pokemon</b>
+        </div>
+        <div className={s.mainText}>You can know the type of Pokemon, its strengths, disadvantages and abilities</div>
+        <Button onClick={(event) => event}>See pokemons</Button>
+      </div>
+      <Parallax />
+    </Layout>
+    <Footer />
   </div>
 );
 
