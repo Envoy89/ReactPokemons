b12a173967d8aa8f163d609275abfec0ae0e09f1
Minyazev Ruzil <ruzil33@gmail.com>
2021-01-26 21:55:07 +0300

    add debounce

--- client/src/pages/Pokedex/index.tsx
+++ client/src/pages/Pokedex/index.tsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
 import useData from '../../hooks/getData';
+import useDebounce from '../../hooks/useDebounce';
 import { IPokemons, IPokemon } from '../../interface/pokedex';
 
 interface IQuery {
+  limit: number;
   name?: string;
 }
 
 const PokedexPage = () => {
   const [searchValue, setSearchValue] = useState<string>('');
-  const [query, setQuery] = useState<IQuery>({});
+  const [query, setQuery] = useState<IQuery>({
+    limit: 12,
+  });
+  const debouncedValue = useDebounce(searchValue, 500);
 
-  const { data, isLoading, isError } = useData<IPokemons>('getPokemons', query, [searchValue]);
+  const { data, isLoading, isError } = useData<IPokemons>('getPokemons', query, [debouncedValue]);
 
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchValue(e.target.value);
