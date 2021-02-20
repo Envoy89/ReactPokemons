d73f48dadfb5a52ca3faa3167e7723b0eaf80f76
Minyazev Ruzil <ruzil33@gmail.com>
2021-02-21 00:21:39 +0300

    Fix not found page

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -15,7 +15,9 @@ const HomePage = () => (
           <b>Find</b> all your favorite <b>Pokemon</b>
         </div>
         <div className={s.mainText}>You can know the type of Pokemon, its strengths, disadvantages and abilities</div>
-        <Button onClick={() => navigate(LinkEnum.POKEDEX)}>See pokemons</Button>
+        <div className={s.button}>
+          <Button onClick={() => navigate(LinkEnum.POKEDEX)}>See pokemons</Button>
+        </div>
       </div>
       <Parallax />
     </Layout>
--- client/src/pages/NotFound/index.tsx
+++ client/src/pages/NotFound/index.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { navigate } from 'hookrouter';
+import { LinkEnum } from '../../routes';
+import Button from '../../components/Button';
 
 import s from './NotFound.module.scss';
 
@@ -6,8 +9,16 @@ import TeamRocketPng from './assets/Team_Rocket.png';
 
 const NotFoundPage = () => (
   <div className={s.root}>
-    <div className={s.notFoundText}>404</div>
+    <div className={s.textContainer}>
+      <div className={s.notFoundText}>404</div>
+      <div className={s.text}>
+        <span className={s.whiteText}>The rocket team</span> has won this time.
+      </div>
+    </div>
     <img className={s.teamRocketPng} src={TeamRocketPng} alt="Cloud PokeBall" />
+    <div className={s.button}>
+      <Button onClick={() => navigate(LinkEnum.HOME)}>Pokemons HOME</Button>
+    </div>
   </div>
 );
 
